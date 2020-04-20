2d3933195f00009c8b24db6e0cef28195ed7b8f6
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-20 14:44:30 -0500

    Se abrevia resultado de promesas a un unico valor que es el esperado

--- algorithms/ejercicios/asincronismoDetails/withPromises.js
+++ algorithms/ejercicios/asincronismoDetails/withPromises.js
@@ -2,61 +2,24 @@ const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 const api = 'https://rickandmortyapi.com/api/character/';
 
-const fetchData = url => {
+const fetchData = url_request => {
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest()
-        xhttp.open('GET', url, true)
+        xhttp.open('GET', url_request)
         xhttp.onreadystatechange = (() => {
             if (xhttp.readyState === 4) {
-                xhttp.status === 200
+                (xhttp.status === 200)
                     ? resolve(JSON.parse(xhttp.responseText))
-                    : reject(new Error('Error', url))
+                    : reject(new Error('Error', url_request))
             }
         })
         xhttp.send()
     })
 }
 
-fetchData(api)
-    .then(results => {
-        console.log(results)
-        return fetchData(`${api}${[results.results[0].id]}`)
-    })
-    .then(origin => {
-        console.log(origin)
-        return fetchData(`${origin.origin.url}`)
-    })
-    .then(dimension => {
-        console.log(dimension.
-const api = 'https://rickandmortyapi.com/api/character/';
 
-        const fetchData = url => {
-            return new Promise((resolve, reject) => {
-                const xhttp = new XMLHttpRequest()
-                xhttp.open('GET', url, true)
-                xhttp.onreadystatechange = (() => {
-                    if (xhttp.readyState === 4) {
-                        xhttp.status === 200
-                            ? resolve(JSON.parse(xhttp.responseText))
-                            : reject(new Error('Error', url))
-                    }
-                })
-                xhttp.send()
-            })
-        }
-
-        fetchData(api)
-            .then(data => {
-                console.log(data)
-                return fetchData(`${api}${[results.results[0].id]}`)
-            })
-            .then(data => {
-                console.log(data)
-                return fetchData(`${api}${origin.origin.url}`)
-            })
-            .then(data => {
-                console.log(data.dimension)
-            })
-            .catch(e => console.log(e))
-    })
-    .catch(e => console.log(e))
+fetchData(API)
+    .then(data => fetchData(`${API}${data.results[0].id}`))
+    .then(character => fetchData(character.origin.url))
+    .then(dimension => console.log(dimension.dimension))
+    .catch(e => { console.log(e) })
