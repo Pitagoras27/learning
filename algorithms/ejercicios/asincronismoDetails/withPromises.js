f76485704fef81fb665643b87fb7fdc277260712
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-20 12:26:22 -0500

    Ejemplo de solicitud a api rick and morty implementando promesas

--- algorithms/ejercicios/asincronismoDetails/withPromises.js
+++ algorithms/ejercicios/asincronismoDetails/withPromises.js
@@ -18,6 +18,45 @@ const fetchData = url => {
 }
 
 fetchData(api)
-    .then((data1) => {
-        console.log(data1)
+    .then(results => {
+        console.log(results)
+        return fetchData(`${api}${[results.results[0].id]}`)
     })
+    .then(origin => {
+        console.log(origin)
+        return fetchData(`${origin.origin.url}`)
+    })
+    .then(dimension => {
+        console.log(dimension.
+const api = 'https://rickandmortyapi.com/api/character/';
+
+        const fetchData = url => {
+            return new Promise((resolve, reject) => {
+                const xhttp = new XMLHttpRequest()
+                xhttp.open('GET', url, true)
+                xhttp.onreadystatechange = (() => {
+                    if (xhttp.readyState === 4) {
+                        xhttp.status === 200
+                            ? resolve(JSON.parse(xhttp.responseText))
+                            : reject(new Error('Error', url))
+                    }
+                })
+                xhttp.send()
+            })
+        }
+
+        fetchData(api)
+            .then(data => {
+                console.log(data)
+                return fetchData(`${api}${[results.results[0].id]}`)
+            })
+            .then(data => {
+                console.log(data)
+                return fetchData(`${api}${origin.origin.url}`)
+            })
+            .then(data => {
+                console.log(data.dimension)
+            })
+            .catch(e => console.log(e))
+    })
+    .catch(e => console.log(e))
