f3d31667d6eb2bffd2cee35cd4e00b9818f9b122
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-20 15:13:39 -0500

    Se refactoriza manejo de promesas para mostrar en un log el valor de la petición final

--- algorithms/ejercicios/asincronismoDetails/withPromises.js
+++ algorithms/ejercicios/asincronismoDetails/withPromises.js
@@ -1,25 +1,10 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const fetchData = require('./fetchData')
 
-const api = 'https://rickandmortyapi.com/api/character/';
-
-const fetchData = url_request => {
-    return new Promise((resolve, reject) => {
-        const xhttp = new XMLHttpRequest()
-        xhttp.open('GET', url_request)
-        xhttp.onreadystatechange = (() => {
-            if (xhttp.readyState === 4) {
-                (xhttp.status === 200)
-                    ? resolve(JSON.parse(xhttp.responseText))
-                    : reject(new Error('Error', url_request))
-            }
-        })
-        xhttp.send()
-    })
-}
-
+const API = 'https://rickandmortyapi.com/api/character/';
 
 fetchData(API)
     .then(data => fetchData(`${API}${data.results[0].id}`))
     .then(character => fetchData(character.origin.url))
     .then(dimension => console.log(dimension.dimension))
     .catch(e => { console.log(e) })
+
