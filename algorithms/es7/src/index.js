7327ad29dabd40d1005db615497b38ad659300c1
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-26 18:01:05 -0500

    incorporaciones es7

---
+++ algorithms/es7/src/index.js
@@ -0,0 +1,12 @@
+// incorporations
+// includes [strings, arrays] en lugar de indexOf
+
+const pairNumbers = [2, 4, 6, 8];
+console.log(pairNumbers.includes(7))
+
+// operador de asignación de exponenciación
+let x = 5
+let y = 3
+
+const result = x ** y
+console.log(result)
