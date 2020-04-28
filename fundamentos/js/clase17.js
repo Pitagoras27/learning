ec04c9ca69f1b8798ae4860ae6e0db0aa00ce86f
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-28 18:35:12 -0500

    Llamados en paralelo y gestión de callbacks por event loop

--- fundamentos/js/clase17.js
+++ fundamentos/js/clase17.js
@@ -1,16 +1,29 @@
 // callbacks
+// EL asincronísmo consiste en como el motor de JS gestiona los tiempos,
+// Las respuestas del servidor varía según muchos factores
+
 
 const API_URL = 'https://pokeapi.co/api/v2/'
 const POKEMON_URL = 'pokemon/:id'
-
-const sobrevaloradoUrl = `${API_URL}${POKEMON_URL.replace(':id', 1)}`
-const opts = { crossDomain: true }
-// en algunos momentos es necesario pasar 
-// esta cabecera
 
 const onPokeResponse = function (pokemon) {
   console.log(`Hola, mi pokemon sobrevalorado es ${pokemon.name}.`)
 }
 
-$.get(sobrevaloradoUrl, {}, onPokeResponse)
+const getCharacter = id => {
+  const sobrevaloradoUrl = `${API_URL}${POKEMON_URL.replace(':id', id)}`
+  const opts = { crossDomain: true }
+  $.get(sobrevaloradoUrl, opts, onPokeResponse)
+}
+
+// llamadas en paraleto
+getCharacter(1)
+getCharacter(2)
+getCharacter(3)
+getCharacter(5)
+// Dado q una función que llama a otra es delegada por el event loop a la
+// cola de tareas, esta va a ser resuelta una vez la pila de ejecución quede
+// liberada
+// cuando la función que hace la solicitud al servidor es liberara del callstack
+// su callback relacionado es pasado de la cola de tareas a la pila de ejecición
 
