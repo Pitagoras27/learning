72f6e38f7d41348f8377873c457662aba4703518
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-28 20:41:42 -0500

    En la parte para resolver las promesas, paso las funciones como nombre de variables y sigue funcionando

--- fundamentos/js/clase19.js
+++ fundamentos/js/clase19.js
@@ -1,17 +1,16 @@
 // Promesas
 const API_URL = 'https://swapi.dev/api/'
 const CHARACTER_URL = 'people/:id'
-
-const swResponse = person => {
-  console.log(`The character of star wars is ${person.name}.`)
-}
-
 const OPTIONS = { crossDomain: true }
-const failErr = () => console.log(`No se pudo obtener el id del personaje ${id}`)
+
+const swResponse = person => console.log(`The character of star wars is ${person.name}.`)
+
+const failErr = id => console.log(`No se pudo obtener el id del personaje ${id}`)
 
 const getCharacter = id => {
   return new Promise((resolve, reject) => {
     const character = `${API_URL}${CHARACTER_URL.replace(':id', id)}`
+
     $
       .get(character, OPTIONS, data => {
         resolve(data)
@@ -22,5 +21,5 @@ const getCharacter = id => {
 
 // Resolve promise
 getCharacter(1)
-  .then(res => swResponse(res))
-  .catch(id => failErr(id))
+  .then(swResponse)
+  .catch(failErr)
