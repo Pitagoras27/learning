e495aeffb1ea80b883caed54fa4ed23f656f34c7
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2021-01-30 19:52:57 -0600

    Se formatea por un código más legible y se agrega método shift para eliminar un elemento al inicio del arreglo

--- algorithms/estructura-de-datos-arrays.js
+++ algorithms/estructura-de-datos-arrays.js
@@ -1,38 +1,42 @@
 class arrayStructure {
-    constructor() {
-        this.length = 0;
-        this.data = {}
-    }
-    
-    get(index) {
-        return this.data[index];
-    }
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
 
-    push(item) {
-        this.data[this.length] = item;
-        this.length +=1
-        return this.data
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length += 1;
+    return this.data;
+  }
+
+  pop() {
+    const itemDelete = this.data[this.lenght - 1];
+    delete this.data[this.lenght - 1];
+    return itemDelete;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.deleteIndex(index);
+    return item;
+  }
+
+  deleteIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
     }
-    
-    pop() {
-        const itemDelete = this.data[this.lenght -1];
-        delete this.data[this.lenght -1];
-        return itemDelete;
-    }
-    
-    delete(index) {
-        const item = this.data[index];
-        this.deleteIndex(index);
-        return item;
-    }
-    
-    deleteIndex(index) {
-        for(let i = index; i < this.length -1; i++) {
-            this.data[i] = this.data[i + 1];
-        }
-        delete this.data[this.length -1];
-        this.length -= 1;
-    }
+    delete this.data[this.length - 1];
+    this.length -= 1;
+  }
+
+  shift() {
+    return this.delete(0);
+  }
 }
 
 const arrayStructureInstance = new arrayStructure();
@@ -44,4 +48,7 @@ arrayStructureInstance.push("D");
 arrayStructureInstance.push("E");
 
 arrayStructureInstance.delete(2);
-console.log(arrayStructureInstance);
+console.log("Delete index two", arrayStructureInstance);
+
+arrayStructureInstance.shift("E");
+console.log("Delete initial index", arrayStructureInstance);
